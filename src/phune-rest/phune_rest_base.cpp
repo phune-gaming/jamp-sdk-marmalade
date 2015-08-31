daefbf3c4a3f6c1602b5a66c4850fe9ab3616919
fjorge <fjorge@fjorges-MBP.lan>
2015-08-31 16:24:12 +0100

    ignoring request if request status is null

--- src/phune-rest/phune_rest_base.cpp
+++ src/phune-rest/phune_rest_base.cpp
@@ -768,7 +768,7 @@ int32 RequestData::GotData(void*, void *userData)
 	// then this will only be called once. However, it may well be
 	// called several times when using chunked encoding.
 
-	if (requestData->http_object == NULL){
+	if (requestData->http_object == NULL || requestData->http_object->GetResponseCode() == NULL){
 		IwTrace(PHUNE, ("IGNORING..."));
 		return 0;
 	}
